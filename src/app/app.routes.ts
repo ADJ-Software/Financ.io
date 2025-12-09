b55d212d86f8d64f7e1386c53d9663384fe62864
Alexandre Machado Rosa Filho <alxmrf04@gmail.com>
2025-12-09 00:41:50 -0300

    chore: configured routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Register } from './pages/auth/register/register';
+import { LandingPage } from './pages/landing-page/landing-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'register', component:Register},
+    {path:"", component:LandingPage}
+];
