3d8348111ca4a1e832f471a1bcb59a5f6891a251
Alexandre Machado Rosa Filho <alxmrf04@gmail.com>
2025-12-11 20:16:05 -0300

    feat: criado o componente de box padrão

--- src/app/pages/test/test-page/test-page.ts
+++ src/app/pages/test/test-page/test-page.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { ThemeToggle } from '../../../components/theme-toggle/theme-toggle';
+import { BoxComponent } from '../../../components/box-component/box-component';
 
 @Component({
   selector: 'app-test-page',
-  imports: [ThemeToggle],
+  imports: [ThemeToggle,BoxComponent],
   templateUrl: './test-page.html',
   styleUrl: './test-page.scss',
 })
